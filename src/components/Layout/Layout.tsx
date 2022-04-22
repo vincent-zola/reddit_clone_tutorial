79c4a8d0e35af1f67631d1db0d8cec337128e05b
vincent-zola <vincent.del@protonmail.com>
2022-04-22 23:48:11 +0200

    test 3

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,14 @@
 //* The Layout component will wrap our entire app in _app.tsx
 // * ========== Imports ==========
 
-import React from "react";
+import React, { ReactNode } from "react";
 import Navbar from "./Navbar/Navbar";
 
-const Layout: React.FC = ({ children }) => {
+interface LayoutProps {
+  children: ReactNode
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   // * ========== HTML ==========
   return (
     <>
