d18f706226f10c5905fe837029a4105ec11f95bf
vincent-zola <vincent.del@protonmail.com>
2022-04-30 17:26:19 +0200

    working on post commponent

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -7,6 +7,9 @@ import { BsLink45Deg, BsMic } from "react-icons/bs";
 import { IoDocumentText, IoImageOutline } from "react-icons/io5";
 import { AiFillCloseCircle } from "react-icons/ai";
 import TabItem from "./TabItem";
+import TextInputs from "./PostForm/TextInputs";
+import ImageUpload from "./PostForm/ImageUpload";
+import { Post } from "../../atoms/postsAtom";
 
 // * ========== TS Types ==========
 
@@ -44,7 +47,56 @@ const formTabs: TabItem[] = [
 ];
 
 const NewPostForm: React.FC<NewPostFormProps> = () => {
+  // we use it to visually highlight the formTabs
   const [selectedTab, setSelectedTab] = useState(formTabs[0].title);
+  const [textInputs, setTextInputs] = useState({
+    title: "",
+    body: "",
+  });
+  const [selectedFile, setSelectedFile] = useState<string>();
+  const [loading, setLoading] = useState(false);
+
+  // *store post in db
+  const handleCreatePost = async () => {
+    // create new post obj => type Post
+    const newPost: Post = {};
+    // store the post in db
+    // check for selectedFile
+    // store in storage => getDownloadURL (return imageURL)
+    // update post doc by adding imageURL
+    // redirect the user back to the communityPage using the router
+  };
+
+  // * image upload Fn.
+  const onSelectImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // FileReader provided by JS
+    const reader = new FileReader();
+    // [0] our Image Input element return an Array of uploaded items in our case one Image, therefore read just the only and first item in array.
+    if (event.target.files?.[0]) {
+      // readAsDataURL() read the binary data and encode it as base64 data url.
+      reader.readAsDataURL(event.target.files[0]);
+    }
+    // store the img in setSelectedFile, readerEvent is just an regular e. event.
+    // .onload: Called when a read operation successfully completes.
+    reader.onload = (readerEvent) => {
+      if (readerEvent.target?.result) {
+        setSelectedFile(readerEvent.target.result as string);
+      }
+    };
+  };
+
+  // * Text and Area recorder Fn.
+  const onTextChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const {
+      target: { name, value },
+    } = event;
+    setTextInputs((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
   // * ========== HTML ==========
 
@@ -59,6 +111,24 @@ const NewPostForm: React.FC<NewPostFormProps> = () => {
             setSelectedTab={setSelectedTab}
           />
         ))}
+      </Flex>
+      <Flex p={4}>
+        {selectedTab === "Post" && (
+          <TextInputs
+            textInputs={textInputs}
+            handleCreatePost={handleCreatePost}
+            onChange={onTextChange}
+            loading={loading}
+          />
+        )}
+        {selectedTab === "Images & Video" && (
+          <ImageUpload
+            selectedFile={selectedFile}
+            onSelectImage={onSelectImage}
+            setSelectedTab={setSelectedTab}
+            setSelectedFile={setSelectedFile}
+          />
+        )}
       </Flex>
     </Flex>
   );
